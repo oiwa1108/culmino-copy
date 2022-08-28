575f6b7590ed43d55c62f3322a80f0d60688b669
daigoro22 <abe.daigoro.sb@tut.jp>
2022-08-29 00:51:10 +0900

    Modified header component with MUI
    MUI でヘッダのコンポーネントを作り直した

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,43 +1,88 @@
-import { Navbar, Nav } from 'react-bootstrap';
-import styles from './style.module.css';
+import MenuIcon from '@mui/icons-material/menu';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  List,
+  ListItem,
+  Box,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React from 'react';
+import { useState } from 'react';
+interface NavParam {
+  text: string;
+  href: string;
+}
+
+const Navigations: React.FC<{ navList: NavParam[] }> = ({ navList }) => (
+  <List>
+    <nav>
+      {navList.map((nav) => (
+        <ListItem>
+          <NavLink href={nav.href}>{nav.text}</NavLink>
+        </ListItem>
+      ))}
+    </nav>
+  </List>
+);
+
+const LogoImg = styled('img')({
+  margin: '0 15px',
+});
+
+const NavLink = styled('a')({
+  margin: '0 15px',
+  fontFamily: 'Yomogi',
+});
 
 export default function Header() {
+  const [expanded, setExpanded] = useState(false);
+
+  const navList: NavParam[] = [
+    { text: 'Culminoについて', href: '#' },
+    { text: '特徴', href: '#' },
+    { text: '簡単ステップ', href: '/step' },
+    { text: '機能について', href: '#' },
+    { text: '料金', href: '#' },
+    { text: '安心安全', href: '#' },
+    { text: 'ダウンロードの仕方', href: '#' },
+    { text: '休会について', href: '#' },
+  ];
   return (
     <>
-      <Navbar
-        collapseOnSelect
-        expand="false"
-        bg="white"
-        variant="light"
-        sticky="top"
-      >
-        <Navbar.Brand href="/">
-          <img
-            className={styles.logo}
-            src="/image/mainlogo.jpg"
-            width="100"
-            height="50"
-            alt="Culminoホーム"
-          />
-        </Navbar.Brand>
-        <div className={styles.humburger}>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        </div>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <div className={styles.navlink}>
-            <Nav className="mr-auto">
-              <Nav.Link href="#">Culminoについて</Nav.Link>
-              <Nav.Link href="#">特徴</Nav.Link>
-              <Nav.Link href="/step">簡単ステップ</Nav.Link>
-              <Nav.Link href="#">機能について</Nav.Link>
-              <Nav.Link href="#">料金</Nav.Link>
-              <Nav.Link href="#">安心安全</Nav.Link>
-              <Nav.Link href="#">ダウンロードの仕方</Nav.Link>
-              <Nav.Link href="#">休会について</Nav.Link>
-            </Nav>
-          </div>
-        </Navbar.Collapse>
-      </Navbar>
+      <Accordion expanded={expanded}>
+        <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+            }}
+          >
+            <LogoImg
+              src="/image/mainlogo.jpg"
+              width="100"
+              height="50"
+              alt="Culminoホーム"
+            />
+          </Box>
+
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+            }}
+          >
+            <MenuIcon onClick={() => setExpanded(!expanded)} />
+          </Box>
+        </AccordionSummary>
+        <AccordionDetails>
+          {<Navigations navList={navList}></Navigations>}
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 }
