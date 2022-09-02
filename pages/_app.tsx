4d8be7f4c342127853f13b9606cbfbf21a5082aa
kento_fujimoto <kentofj121600@gmail.com>
2022-09-02 21:47:36 +0900

    bootstrap、Providerを適応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import '../styles/globals.css';
+import reduxStore from 'redux/store';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import theme from '../src/theme';
 
 function MyApp({ Component, pageProps }) {
+  const { store, persistor } = reduxStore();
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
