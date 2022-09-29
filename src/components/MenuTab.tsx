81c0544d99108d66d6daac236e913a3b5e3e778a
kento_fujimoto <kentofj121600@gmail.com>
2022-09-29 17:00:50 +0900

    ファイル分割

--- src/components/MenuTab.tsx
+++ src/components/MenuTab.tsx
@@ -2,34 +2,12 @@ import React, { ReactNode } from 'react';
 import Paper from '@mui/material/Paper';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
 import styles from '@styles/Home.module.css';
-
-type TabPanelProps = {
-  children: ReactNode;
-  value: number;
-  index: number;
-};
+import { TabPanel } from '../components/TabPanel';
 
 type MenuTabProps = {
   labels: string[];
   children: ReactNode[];
-};
-
-const TabPanel = (props: TabPanelProps) => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </div>
-  );
 };
 
 export function MenuTab(props: MenuTabProps) {
