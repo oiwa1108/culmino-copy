4786159148063378ae9e188489eff8a225fbe66e
kento_fujimoto <kentofj121600@gmail.com>
2022-10-27 00:36:21 +0900

    登録完了画面の実装

---
+++ pages/complete.tsx
@@ -0,0 +1,32 @@
+import Header from '../src/components/header';
+import Head from '../src/components/head';
+import { Typography, Box, Button } from '@mui/material';
+import { useRouter } from 'next/router';
+
+export default function Complete() {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head />
+      <Box sx={{ maxWidth: '375px', margin: '0 auto' }}>
+        <Header />
+        <Box sx={{ textAlign: 'center', paddingY: 4, paddingX: 2 }}>
+          <Typography sx={{ fontWeight: 600 }}>
+            会員登録完了ありがとうございます
+          </Typography>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{ fontWeight: 600, mt: 4 }}
+            onClick={() => {
+              router.push('/home');
+            }}
+          >
+            ホームへ
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+}
