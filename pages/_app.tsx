e52d0fe07ec39299b49b319fcdc36aa992af6564
kento_fujimoto <kentofj121600@gmail.com>
2022-10-27 17:08:47 +0900

    全体のwidth修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,21 @@
 import '../styles/globals.css';
 import reduxStore from 'redux/store';
 import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Box } from '@mui/material';
 import { Provider } from 'react-redux';
 
 import theme from '../src/theme';
 
 function MyApp({ Component, pageProps }) {
-  const { store, persistor } = reduxStore();
+  const { store } = reduxStore();
 
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Component {...pageProps} />
+        <Box sx={{ maxWidth: '500px', margin: '0 auto' }}>
+          <Component {...pageProps} />
+        </Box>
       </ThemeProvider>
     </Provider>
   );
